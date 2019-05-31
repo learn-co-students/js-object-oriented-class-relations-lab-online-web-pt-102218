e7885fedaf75658598f762bf8f8554f4f60ae647
Johnathon Wood <jaywood128@gmail.com>
2019-05-31 16:04:58 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+let driverId = 0; 
+
+let store = { drivers: [], passengers: [], trips: []}; 
+
+class Driver {
+  constructor(name) { 
+    this.id = ++ driverId; 
+    this.name = name; 
+
+    store.drivers.push(this);
+  }
+}
+
+let passengerId = 0; 
+
+class Passenger {
+  constructor(name) {
+    this.id = ++ passengerId;
+    this.name = name; 
+
+    store.passengers.push(this);
+  }
+}
+
+let tripId = 0; 
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++ tripId;
+    this.driverId = driver.id; 
+    this.passengerId = passenger.id;
+
+    store.trips.push(this);
+  };
+  passenger() {
+    return store.passengers.filter(
+        function(passenger) {
+          return passenger.id === this.passengerId;
+        }.bind(this)
+    );
+  }
+}
